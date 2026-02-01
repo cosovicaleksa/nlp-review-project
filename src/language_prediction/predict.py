05fe2926489445184ca8085c739965b0821bb855
cosovicaleksa <cosovic.aleksa021@gmail.com>
2026-02-01 22:43:11 +0100

    Remove fastText-based language checking

--- src/language_prediction/predict.py
+++ src/language_prediction/predict.py
@@ -1,11 +1,6 @@
 from src.artifacts import load_pickle_cached
 from src.config import LANG_PRED_MODEL_PATH, LANG_PRED_VECTORIZER_PATH, reverse_mapping
 
-import fasttext
-from huggingface_hub import hf_hub_download
-
-_fasttext_cache = None
-
 SUPPORTED_LANGS = {
     "__label__eng_Latn",
     "__label__deu_Latn",
@@ -13,27 +8,8 @@ SUPPORTED_LANGS = {
     "__label__spa_Latn",
 }
 
-def get_fasttext():
-    global _fasttext_cache
-    if _fasttext_cache is None:
-        model_path = hf_hub_download(repo_id="facebook/fasttext-language-identification", filename="model.bin")
-        _fasttext_cache = fasttext.load_model(model_path)
-    return _fasttext_cache
-
-# ovo korisitm samo za proveru da li je review na nekom od jezika na kojem nije trenian moj model
-def check_language_label(user_review: str):
-    ft = get_fasttext()
-    labels, probs = ft.predict(user_review, k=1)
-    return labels[0], float(probs[0])   
-
 def predict_language(user_review: str):
-    ft_label, ft_prob = check_language_label(user_review)
 
-    # If fastText is confident AND says it's not one of supported languages -> Unsupported
-    if ft_prob > 0.8 and ft_label not in SUPPORTED_LANGS:
-        return user_review, "Unsupported"
-
-    # Otherwise use your 4-class model
     model = load_pickle_cached(LANG_PRED_MODEL_PATH)
     vectorizer = load_pickle_cached(LANG_PRED_VECTORIZER_PATH)
 
--- src/main.py
+++ src/main.py
@@ -1,28 +1,28 @@
 from src.language_prediction.predict import predict_language
 from src.star_predicton.predict import predict_star
 from src.machine_translation.translation import translate
+from src.clustering.predict import predict_cluster
 
 def main():
 
     user_review = input('Enter review: ')
     user_review, predicted_language = predict_language(user_review)
 
-    if predicted_language == 'Unsupported':
-        print("Unsupported language. Review must be in English, German, French, or Spanish.")
-    else:
-        # Correct language
-        print(predicted_language)
+    # Correct language
+    print(f"\n Detected language:\n→ {predicted_language}\n")
 
-        # Star pred
-        predicted_star = predict_star(user_review, predicted_language)
-        print(predicted_star)
+    # Star pred
+    predicted_star = predict_star(user_review, predicted_language)
+    print(f"Predicted rating:\n→ {predicted_star} stars\n")
 
-        if predicted_language != 'English':
-            translated_review = translate(user_review, predicted_language)
-        else:
-            translated_review = user_review
+    if predicted_language != 'English':
+        user_review = translate(user_review, predicted_language)
+        print(f"Translated review (English):\n→ {user_review}\n")
+    
+    predicted_cluster = predict_cluster(user_review)
+    print(f"Predicted cluster:\n→ {predicted_cluster}\n")
 
-        print(translated_review)
+        
 
 if __name__ == "__main__":
     main()
